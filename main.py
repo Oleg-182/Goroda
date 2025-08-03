d1e9c92c10c604a88bf61e4a69bbc7acc54a124d
Oleg-192 <starter-nsk@yande[.ru>
2025-08-03 22:20:19 +0700

    Добавлены широта и долгота

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ def get_coordinates(city, key):
         if results:
             lat = round(results[0]['geometry']['lat'], 2) # округляем до 2-ч знаков
             lon = round(results[0]['geometry']['lng'], 2) # округляем до 2-ч знаков
-            return lat, lon
+            return f'Широта: {lat}, Долгота: {lon}'
         else:
             return "Город не найден"
     except Exception as e:
         return f'Возникла ошибка: {e}'
 
 key = 'a036f38429b74435af47ce1876f839d3'
-city = 'Новосибирск'
+city = '"Эквадор"'
 
 coordinates = get_coordinates(city, key)
 
